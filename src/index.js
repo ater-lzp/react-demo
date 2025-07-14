fa7aca3c9ce17461714898abf1f482760c9c1683
ater <2083642787@qq.com>
2025-07-14 16:17:59 +0800

    fix(ui): 修复卖座选项卡组件的导入问题
    - 将注释掉的 `import App from "./01-base/12-卖座选项卡";` 取消注释，以正确导入卖座选项卡组件。
    - 将 `import App from "./01-base/19-受控组件/Father";` 注释掉，以避免与卖座选项卡组件冲突。
    
    Closes #123

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: ater
  * @Date:   2025-07-08 10:52:25
  * @Last Modified by:   ater
- * @Last Modified time: 2025-07-14 15:41:11
+ * @Last Modified time: 2025-07-14 16:11:41
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -11,8 +11,8 @@ import ReactDOM from "react-dom/client";
 // import App from "./01-base/03-组件的嵌套";
 // import App from "./01-base/04-组件的样式";
 // import App from "./01-base/05-事件绑定";
-// import App from "./01-base/12-卖座选项卡";
+import App from "./01-base/12-卖座选项卡";
 // import App from "./01-base/16-props/defineProps";
-import App from "./01-base/19-受控组件/Father";
+// import App from "./01-base/19-受控组件/Father";
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
